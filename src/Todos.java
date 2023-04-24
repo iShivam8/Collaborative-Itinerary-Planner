fa3245ceb64c032c497bf8521e3a6f45b27b0f90
Shivam Shukla <shivam.p.shukla@medtronic.com>
2023-04-24 18:34:08 -0400

    Added TODOs

---
+++ src/Todos.java
@@ -0,0 +1,15 @@
+public class Todos {
+
+  // TODO - Remove src/ from every logs creator
+
+  // TODO - Main menu does not pop up after every operation
+  // TODO - Implement Profile Options
+  // TODO - Email Validate
+  // TODO - EDIT functionality. Only Owner and Shared users can edit the itinerary
+  // TODO - Servers are not in sync
+  // TODO - Change Unique ID generated format
+  // TODO - Rename Method description in PaxosServer. Add edit, share
+  // TODO - Remove the JSON value from paxos
+  // TODO - Update the version count after an edit is made
+  // TODO - Shared users are not visible after Share operation is executed
+}
